b9f28b23186d4bc5eb91bbbcba886c0ae6170dc4
Keisuke <keisuke.lester@my.utsa.edu>
2022-09-10 01:32:08 -0500

    changed Jframe sizing

--- src/GenshinManagementSystem/GenshinManagementSystem.java
+++ src/GenshinManagementSystem/GenshinManagementSystem.java
@@ -32,14 +32,15 @@ public class GenshinManagementSystem extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 1460, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 800, Short.MAX_VALUE)
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     /**
